37858484fe78305d7da3f75aad70ad3799f8e5e3
funky81 <funky81.android@gmail.com>
2012-06-12 04:39:41 +0700

    Linux 3.0.17
    Changelog:
    http://www.kernel.org/pub/linux/kernel/v3.0/ChangeLog-3.0.17

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 19 */
+/* This file is auto generated, version 25 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#19 SMP PREEMPT Tue Jun 12 03:00:18 WIT 2012"
+#define UTS_VERSION "#25 SMP PREEMPT Tue Jun 12 04:19:44 WIT 2012"
 #define LINUX_COMPILE_BY "hardcore-funky81"
 #define LINUX_COMPILE_HOST "speedmod-n7000-ics"
 #define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.16-g99d91d7-dirty"
+#define UTS_RELEASE "3.0.16-g02ef57e-dirty"
