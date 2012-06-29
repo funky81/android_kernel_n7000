044a98a0a8105fd8adf02be1f03627498eaaf782
Dorimanx <yuri@bynet.co.il>
2012-06-29 23:49:41 +0300

    LINUX 3.0.34 Missing changes!
    Conflicts:
    
    	arch/parisc/kernel/entry.S

--- drivers/gpu/drm/radeon/rv770.c
+++ drivers/gpu/drm/radeon/rv770.c
@@ -194,6 +194,8 @@ void rv770_pcie_gart_disable(struct radeon_device *rdev)
 	WREG32(MC_VM_MD_L1_TLB0_CNTL, tmp);
 	WREG32(MC_VM_MD_L1_TLB1_CNTL, tmp);
 	WREG32(MC_VM_MD_L1_TLB2_CNTL, tmp);
+	if (rdev->family == CHIP_RV740)
+		WREG32(MC_VM_MD_L1_TLB3_CNTL, tmp);
 	WREG32(MC_VM_MB_L1_TLB0_CNTL, tmp);
 	WREG32(MC_VM_MB_L1_TLB1_CNTL, tmp);
 	WREG32(MC_VM_MB_L1_TLB2_CNTL, tmp);
